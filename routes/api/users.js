48f9d3c0837cb2ec4fd524f016f62ad8a4c72fee
hp <teddybear90@abv.bg>
2018-06-30 22:39:30 +0300

    change

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+// const gravatar = require('gravatar');
+
+
+
+// Load User model
+const User = require('../../models/User');
+
 
 // route   GET api/users/test
 // desc    Tests users route
 // access  Public
 router.get('/test', (req, res) => res.json({msg:"users works"}))
+
+
 module.exports = router
