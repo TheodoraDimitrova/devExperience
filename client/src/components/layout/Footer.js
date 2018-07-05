2810bef9a17cc63f47dfc835db37e0078e2cfcce
hp <teddybear90@abv.bg>
2018-07-05 21:47:44 +0300

    Basic Layout

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <footer class="bg-dark text-white mt-5 p-4 text-center">
+    Copyright &copy; 2018 Dev Experiment
+  </footer>
+  )
+}
+
