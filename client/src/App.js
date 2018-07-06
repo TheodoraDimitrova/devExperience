9312f43015cb93d9b63184579e55c918b688788e
hp <teddybear90@abv.bg>
2018-07-07 00:41:40 +0300

    import router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Navbar />
         <Landing />
         <Footer />
       </div>
+      </Router>
     );
   }
 }
