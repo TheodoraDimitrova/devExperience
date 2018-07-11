e807485fe8489223de104d679688354076148397
hp <teddybear90@abv.bg>
2018-07-11 12:16:55 +0300

    redux login action and set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import { GET_ERRORS ,SET_CURRENT_USER} from "./types";
+import jwt_decode from "jwt-decode";
 
 //register
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +14,36 @@ export const registerUser = (userData, history) => dispatch => {
         playload: err.response.data
       })
     );
+};
+
+//Login get user token
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //save to localStorage
+      const { token } = res.data;
+      //set token to local storage
+      localStorage.setItem("jwtToken", token);
+      //set token to Auth header
+      setAuthToken(token);
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        playload: err.response.data
+      })
+    );
+};
+//set login user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    playload: decoded
+  };
 };
