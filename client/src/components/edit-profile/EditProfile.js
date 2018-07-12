5dd55917acb471bc1f02dc2b393fc5743afa6e3f
hp <teddybear90@abv.bg>
2018-07-12 18:34:18 +0300

    edit profile component

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -192,7 +192,7 @@ class CreateProfile extends Component {
                   value={this.state.handle}
                   onChange={this.onChange}
                   error={errors.handle}
-                  info="A unique handle for your profile URL. Your full name, company name, nickname"
+                  info="A unique handle for your profile URL. Your full name or nickname"
                 />
                 <SelectListGroup
                   placeholder="Status"
