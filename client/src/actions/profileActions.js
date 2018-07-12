dbadf7cd480e449372ad9fe690ac9829670acdac
hp <teddybear90@abv.bg>
2018-07-12 17:40:37 +0300

    delete account action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_PROFILE, CLEAR_CURRENT_PROFILE, GET_ERRORS } from "./types";
+import { GET_PROFILE, CLEAR_CURRENT_PROFILE, GET_ERRORS ,SET_CURRENT_USER } from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -36,4 +36,23 @@ export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
   };
+};
+
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This can NOT be restored!')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
