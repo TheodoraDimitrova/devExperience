90fb01f1a7f350724e931e7bc350cdecca811702
hp <teddybear90@abv.bg>
2018-07-12 17:39:55 +0300

    add delete account button added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,16 +2,22 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { getCurrentProfile } from "../../actions/profileActions";
+import { getCurrentProfile ,deleteAccount } from "../../actions/profileActions";
+import ProfileActions from './ProfileActions';
+// import Experience from './Experience';
+// import Education from './Education';
 
 class Dashboard extends Component {
   componentDidMount() {
-    if (!this.props.auth.isAuthenticated) {
-      this.props.history.push("/login");
-    }
+    // if (this.props.auth.isAuthenticated===false) {
+    //   this.props.history.push("/login");
+    // }
     this.props.getCurrentProfile();
   }
-
+  
+  onDeleteClick(e) {
+    this.props.deleteAccount();
+  }
   render() {
     const { user } = this.props.auth;
     const { profile } = this.props.profile;
@@ -23,7 +29,23 @@ class Dashboard extends Component {
       dashboardContent = <h4>Please login or register</h4>;
     } else {
       if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>;
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">
+              Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+            </p>
+            <ProfileActions />
+            {/* <Experience experience={profile.experience} />
+            <Education education={profile.education} /> */}
+            <div style={{ marginBottom: '60px' }} />
+            <button
+              onClick={this.onDeleteClick.bind(this)}
+              className="btn btn-danger"
+            >
+              Delete My Account
+            </button>
+          </div>
+        );
       } else {
         //User is logged in but has no profile
         dashboardContent = (
@@ -55,6 +77,7 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount:PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 };
@@ -66,5 +89,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { getCurrentProfile }
+  { getCurrentProfile,deleteAccount }
 )(Dashboard);
