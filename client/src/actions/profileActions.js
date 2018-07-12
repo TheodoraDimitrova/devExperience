bd337c26d60c79afd1dd91fc50bdb8246966cbc4
hp <teddybear90@abv.bg>
2018-07-12 21:16:43 +0300

    actions delete education and experience

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -79,3 +79,37 @@ export const addEducation = (eduData, history) => dispatch => {
       })
     );
 };
+
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
