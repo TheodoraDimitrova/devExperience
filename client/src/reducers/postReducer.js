a6c9d5001dbf5571c88225a8931237cc8c9abee9
hp <teddybear90@abv.bg>
2018-07-14 13:13:34 +0300

    added cases for posts

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,8 +1,8 @@
 import {
-    ADD_POST
-    // GET_POSTS,
-    // GET_POST,
-    // DELETE_POST
+    ADD_POST,
+    GET_POSTS,
+    GET_POST,
+    DELETE_POST
   } from '../actions/types';
   
   const initialState = {
@@ -13,28 +13,28 @@ import {
   
   export default function(state = initialState, action) {
     switch (action.type) {
-      // case GET_POSTS:
-      //   return {
-      //     ...state,
-      //     posts: action.payload,
+      case GET_POSTS:
+        return {
+          ...state,
+          posts: action.payload,
          
-      //   };
-      // case GET_POST:
-      //   return {
-      //     ...state,
-      //     post: action.payload,
+        };
+      case GET_POST:
+        return {
+          ...state,
+          post: action.payload,
           
-      //   };
+        };
       case ADD_POST:
         return {
           ...state,
           posts: [action.payload, ...state.posts]
         };
-      // case DELETE_POST:
-      //   return {
-      //     ...state,
-      //     posts: state.posts.filter(post => post._id !== action.payload)
-      //   };
+      case DELETE_POST:
+        return {
+          ...state,
+          posts: state.posts.filter(post => post._id !== action.payload)
+        };
       default:
         return state;
     }
