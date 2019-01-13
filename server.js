85544557b22c36d317d8a2a57b3c042dc52cddcb
hp <teddybear90@abv.bg>
2019-01-13 16:58:54 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use("/api/posts", posts);
 //server in production
 if(process.env.NODE_ENV === 'production'){
   //Static folder
-  app.use(express.static('client/build'))
+  app.use(express.static('client/build'));
 
   app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
