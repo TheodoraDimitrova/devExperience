1cbf91663148dd8df89a62e14e7322b5ca641ec3
teodora <tbd@shippii.com>
2025-04-12 10:27:52 +0300

    Fix production build and static file serving

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello teddy");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello teddy");
+// });
 
 //Passport middleware
 app.use(passport.initialize());
@@ -37,14 +37,14 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 //server in production
-// if(process.env.NODE_ENV === 'production'){
-//Static folder
-app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  //Static folder
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-// }
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
