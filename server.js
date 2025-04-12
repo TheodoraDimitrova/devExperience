1522be064d89a150e9d11c4bb22d7cc822d726e4
teodora <tbd@shippii.com>
2025-04-12 09:52:17 +0300

    server in production

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path=require('path')
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -20,7 +20,7 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello teddy");
@@ -37,14 +37,14 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 //server in production
-if(process.env.NODE_ENV === 'production'){
-  //Static folder
-  app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production'){
+//Static folder
+app.use(express.static("client/build"));
 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-  })
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+// }
 
 const port = process.env.PORT || 5000;
 
